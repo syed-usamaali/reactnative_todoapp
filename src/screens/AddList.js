131818719b908a2ff913a2b38878b37d52fb0cde
syed-usamaali <58572485+syed-usamaali@users.noreply.github.com>
2020-02-24 02:41:06 +0500

    add todo list

---
+++ src/screens/AddList.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class AddList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Add Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default AddList;
