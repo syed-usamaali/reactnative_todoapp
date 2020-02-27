0cb66adff1f04c5dfecad0ccb789bde69f44cf3c
syed-usamaali <58572485+syed-usamaali@users.noreply.github.com>
2020-02-28 02:52:34 +0500

    added todo screen

--- src/screens/TodoList.js
+++ src/screens/TodoList.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
+import Header from '../components/Header';
 
 class TodoList extends Component {
   render() {
     return (
-      <View>
+      <View style={styles.containerStyle}>
+        <Header headerText="Todo" />
         <Text>List Screen</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    backgroundColor: '#FFF',
+    height: '100%',
+  },
+});
 
 export default TodoList;
