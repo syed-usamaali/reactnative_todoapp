cddeb36222e9fa8e0ee5cb26f32e2ee70296dd6c
syed-usamaali <58572485+syed-usamaali@users.noreply.github.com>
2020-02-28 02:51:02 +0500

    added components

--- src/screens/AddList.js
+++ src/screens/AddList.js
@@ -1,14 +1,42 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
+import Header from '../components/Header';
 
 class AddList extends Component {
   render() {
     return (
-      <View>
-        <Text>Add Screen</Text>
+      <View style={styles.containerStyle}>
+        <Header headerText="Add" />
+        <View style={styles.viewStyle}>
+          <TextInput
+            style={styles.textInputStyle}
+            placeholder="What do you want to do?"
+            multiline={true}
+            textAlignVertical={'top'}
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    backgroundColor: '#FFF',
+    height: '100%',
+  },
+  viewStyle: {
+    marginHorizontal: 18,
+    marginTop: 20,
+  },
+  textInputStyle: {
+    borderWidth: 1,
+    borderColor: '#d6d4cb',
+    borderRadius: 5,
+    fontSize: 16,
+    paddingHorizontal: 10,
+    height: 125,
+  },
+});
 
 export default AddList;
